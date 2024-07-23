356fe99c6e1aacc027eb04016f8d7cfe89c74fc1
Enzo Marques <enzomarques94500@gmail.com>
2024-07-23 12:26:18 +0200

    Update + try

--- Portfolio/javascript/projets.js
+++ Portfolio/javascript/projets.js
@@ -1,21 +1,3 @@
-// Setting up the Variables
-var bars = document.getElementsByClassName("menu-btn");
-var nav = document.getElementsByClassName("menu");
-
-console.log(bars);
-
-//setting up the listener
-bars[0].addEventListener("click", barClicked, false);
-
-//setting up the clicked Effect
-function barClicked() {
-  bars[0].classList.toggle('open');
-  nav[0].classList.toggle('menu-open');
-}
-
-
-// SLIDE 
-
 const slider = document.querySelector('.slider');
 
 function activate(e) {
@@ -26,28 +8,3 @@ function activate(e) {
 
 document.addEventListener('click',activate,false);
 
-// SLIDE
-
-// DOWNLOAD
-
-
-// document.getElementById('afficherCV').addEventListener('click', function() {
-//   var zoneCV = document.getElementById('zoneCV');
-//   var pdfPath = 'CV ALTERNANCE.pdf'; // Remplacez par le chemin vers votre CV
-
-//   // Créer un objet <embed> pour afficher le PDF
-//   var embedElement = document.createElement('embed');
-//   embedElement.setAttribute('src', pdfPath);
-//   embedElement.setAttribute('type', 'application/pdf');
-//   embedElement.setAttribute('width', '100%');
-//   embedElement.setAttribute('height', '600px');
-
-//   // Ajouter l'objet <embed> à la zone du CV
-//   zoneCV.innerHTML = ''; // Effacer le contenu précédent, s'il y en a
-//   zoneCV.appendChild(embedElement);
-
-//   // Afficher la zone du CV
-//   zoneCV.style.display = 'block';
-// });
-
-// DOWNLOAD
